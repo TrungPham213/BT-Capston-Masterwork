d098aacd64fd8dd3934d7ea7cafbd8f5aaa12301
TrungPham213 <mittotbung@gmail.com>
2023-04-21 12:11:00 +0700

    upload code

--- README.md
+++
@@ -1,1 +0,0 @@
-# BT-Capston-Masterwork
--- js/main.js
+++ js/main.js
@@ -1,31 +1,23 @@
-(function(){
-    // Back to Top - by CodyHouse.co
-	var backTop = document.getElementsByClassName('js-cd-top')[0],
-		offset = 300, // browser window scroll (in pixels) after which the "back to top" link is shown
-		offsetOpacity = 1200, //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
-		scrollDuration = 700,
-		scrolling = false;
+// Switch theme
+document.getElementById('btnSwitchDark').onclick = function(){
+    document.getElementById('body').className = 'switch-theme';
+    document.getElementById('btnSwitchDark').style.display = 'none';
+    document.getElementById('btnSwitchLight').style.display = 'inline-block';
+};
 
-	if( backTop ) {
-		//update back to top visibility on scrolling
-		window.addEventListener("scroll", function(event) {
-			if( !scrolling ) {
-				scrolling = true;
-				(!window.requestAnimationFrame) ? setTimeout(checkBackToTop, 250) : window.requestAnimationFrame(checkBackToTop);
-			}
-		});
+document.getElementById('btnSwitchLight').onclick = function(){
+    document.getElementById('body').classList.remove('switch-theme');
+    document.getElementById('btnSwitchDark').style.display = 'inline-block';
+    document.getElementById('btnSwitchLight').style.display = 'none';
+};
 
-		//smooth scroll to top
-		backTop.addEventListener('click', function(event) {
-			event.preventDefault();
-			(!window.requestAnimationFrame) ? window.scrollTo(0, 0) : Util.scrollTo(0, scrollDuration);
-		});
-	}
+// Show/Hide button in header
+document.getElementById('btnOpen').onclick = function(){
+    document.getElementById('btnOpen').style.display = 'none';
+    document.getElementById('btnClose').style.display = 'block';
+};
 
-	function checkBackToTop() {
-		var windowTop = window.scrollY || document.documentElement.scrollTop;
-		( windowTop > offset ) ? Util.addClass(backTop, 'cd-top--is-visible') : Util.removeClass(backTop, 'cd-top--is-visible cd-top--fade-out');
-		( windowTop > offsetOpacity ) && Util.addClass(backTop, 'cd-top--fade-out');
-		scrolling = false;
-	}
-})();
+document.getElementById('btnClose').onclick = function(){
+    document.getElementById('btnClose').style.display = 'none';
+    document.getElementById('btnOpen').style.display = 'block';
+};
